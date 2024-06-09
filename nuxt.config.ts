d544bbf7fd95e15fb971fefec7f2b51f643c3131
Paweł Żentała <zentala@users.noreply.github.com>
2024-06-09 17:50:47 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@vueuse/nuxt'
+  ],
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+    safelistColors: ['primary', 'red', 'orange', 'green']
+  },
+  colorMode: {
+    disableTransition: true
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
